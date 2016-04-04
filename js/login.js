296c96ad68416bf3176ac25e1911a7b088b5873d
Noelle Knight <noelle.knight@gmail.com>
2016-04-04 16:59:14 -0400

    ajax request for user data, put data in object

--- js/login.js
+++ js/login.js
@@ -2,20 +2,35 @@
   'use strict';
 
   ns.gitToken = '';
+  ns.userInfo = {};
 
-  // event handler for login + Ajax call to retrieve profile data
+  // event handler for login
 
   $('#loginForm').on('submit', function (event){
     event.preventDefault();
     ns.gitToken = $('#loginInput').val();
-  
 
-    // $.ajax({
-    //   type: 'GET',
-    //   url: 'https://api.github.com/user',
-    //   dataype: 'json',
-    //
-    // });
+// Ajax call to retrieve profile data, in success data put in object
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.github.com/user',
+      dataype: 'json',
+      headers: {
+          authorization: 'token ' + ns.gitToken
+        },
+      success: function userData (data){
+      ns.userInfo.Username = data.login;
+      ns.userInfo.Name = data.name;
+      ns.userInfo.Repos = data.public_repos;
+      ns.userInfo.Followers = data.followers;
+      ns.userInfo.Created = data.created_at;
+
+      console.log(ns.userInfo);
+      // {Username: data.login, Name: data.name, Repos: data.public_repos, Followers: data.followers, Account created: data.created_at};
+
+    }
+    });
 
   });
   window.gitTracker = ns;
