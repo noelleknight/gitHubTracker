10afc781db045ed0db2d910e3d9930e9a0e6fbbc
Noelle Knight <noelle.knight@gmail.com>
2016-04-04 16:08:45 -0400

    eventhandler for login and var created for token

--- js/login.js
+++ js/login.js
@@ -1,12 +1,23 @@
 (function(ns) {
   'use strict';
 
+  ns.gitToken = '';
 
+  // event handler for login + Ajax call to retrieve profile data
 
+  $('#loginForm').on('submit', function (event){
+    event.preventDefault();
+    ns.gitToken = $('#loginInput').val();
+  
 
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'https://api.github.com/user',
+    //   dataype: 'json',
+    //
+    // });
 
-
-
-window.gitTracker = ns;
+  });
+  window.gitTracker = ns;
 
 })(window.gitTracker || {});
